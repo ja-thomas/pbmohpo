7185e51e2e3f29239d7a1829c18c639ebfc0493f
Janek Thomas <janek.tho@gmail.com>
2023-01-24 16:21:24 +0100

    command line arguments for examples

--- minimal_example.py
+++ minimal_example.py
@@ -1,3 +1,5 @@
+import argparse
+
 from pbmohpo.benchmark import Benchmark
 from pbmohpo.decision_makers.decision_maker import DecisionMaker
 from pbmohpo.optimizers.random_search import RandomSearch
@@ -5,26 +7,42 @@ from pbmohpo.optimizers.utility_bayesian_optimization import UtilityBayesianOpti
 from pbmohpo.problems.yahpo import YAHPO
 from pbmohpo.problems.zdt1 import ZDT1
 
-dim = 10
-budget = 50
+parser = argparse.ArgumentParser(description="Run some examples!")
 
-print("Testing ZDT1")
+parser.add_argument("--problem", choices=["zdt1", "yahpo"], default="zdt1")
+parser.add_argument("--budget", type=int, default=50)
+parser.add_argument("--optimizer", choices=["RS", "BO"], default="BO")
+parser.add_argument("--dimension", type=int, default=10)
 
-prob = ZDT1(dimension=dim)
+args = parser.parse_args()
+
+budget = args.budget
+optimizer = args.optimizer
+problem = args.problem
+
+if args.problem == "zdt1":
+    print("Testing ZDT1")
+    prob = ZDT1(dimension=args.dimension)
+else:
+    print("Testing YAHPO")
+    prob = YAHPO(
+        id="lcbench", instance="3945", objective_names=["time", "val_accuracy"]
+    )
+
+if args.optimizer == "RS":
+    print("Running Random Search")
+    opt = RandomSearch(prob.get_config_space())
+else:
+    print("Running Bayesian Optimization on Utility Scores")
+    opt = UtilityBayesianOptimization(prob.get_config_space())
+
 dm = DecisionMaker(objective_names=prob.get_objective_names())
-opt = UtilityBayesianOptimization(prob.get_config_space())
+
+print("Decision Maker Preference Scores:")
+print(dm.preferences)
 
 bench = Benchmark(prob, opt, dm, budget)
 bench.run()
 
-
-print("Testing YAHPO")
-
-targets = ["time", "val_accuracy"]
-
-prob = YAHPO(id="lcbench", instance="3945", objective_names=targets)
-dm = DecisionMaker(objective_names=prob.get_objective_names())
-opt = UtilityBayesianOptimization(prob.get_config_space())
-
-bench = Benchmark(prob, opt, dm, budget)
-bench.run()
+print(f"Best Configuration found in iteration [{bench.archive.incumbents[0]}]:")
+print(bench.archive.data[bench.archive.incumbents[0]])
