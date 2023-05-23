5f64f8c8c44dfda4db4c925c383ad0b929e9e4be
Lennart Schneider <lennart.sch@web.de>
2023-05-23 21:32:14 +0200

    tests

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,5 +1,15 @@
 from pbmohpo.benchmark import *
+from pbmohpo.decision_makers.decision_maker import DecisionMaker
+from pbmohpo.optimizers.random_search import RandomSearch
+from pbmohpo.problems.zdt1 import ZDT1
 
 def test_benchmark():
-    expect benchmark = Benchmark()
+    problem = ZDT1(dimension=2)
+    optimizer = RandomSearch(problem.get_config_space())
+    decision_maker = DecisionMaker(objective_names=problem.get_objective_names(), seed=0)
+    benchmark = Benchmark(problem=problem, optimizer=optimizer, dm=decision_maker, eval_budget=10, dm_budget=10, eval_batch_size=1, dm_batch_size=1)
     assert type(benchmark) == Benchmark
+    assert type(benchmark.archive) == Archive
+    assert benchmark.archive.evaluations == []
+    benchmark.run()
+    assert len(benchmark.archive.evaluations) == 10
