fix: proper treating of log search spaces, make acqf optimization more robust, use EI for utility based BO, update templates of yahpo iaml to include objective scaling, minimal other changes

pbmohpo/optimizers/optimizer.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 from typing import List, Tuple
 
 import ConfigSpace as CS
+import numpy as np
 from torch import Tensor
 
 from pbmohpo.archive import Archive
@@ -119,7 +120,7 @@ class BayesianOptimization(Optimizer):
         return configs
 
     def _candidates_to_configs(
-        self, candidates: Tensor, n: int
+        self, candidates: Tensor, n: int, on_search_space: bool = True
     ) -> List[CS.Configuration]:
         """
         Convert a tensor of candidates found by optimize_acqf to a list of configurations
@@ -131,6 +132,8 @@ class BayesianOptimization(Optimizer):
             Candidate tensor
         n: int
             Number of configurations to propose in one batch
+        on_search_space: bool
+            Whether candidates are on the search space, i.e. respecting log transformations
 
         Returns
         -------
@@ -150,7 +153,11 @@ class BayesianOptimization(Optimizer):
 
             # candidate contains only floats, round integer HPs
             for hp, val in zip(hp_names, hp_values):
-                if not self.config_space.get_hyperparameter(hp).is_legal(val):
+                if on_search_space:
+                    if self.config_space.get_hyperparameter(hp).log:
+                        val = np.exp(val)
+                # hack to round integers because there is no direct query method for the type of a hyperparameter
+                if isinstance(self.config_space.get_hyperparameter(hp), CS.hyperparameters.IntegerHyperparameter):
                     val = round(val)
                 config_dict[hp] = val
             configurations.append(CS.Configuration(self.config_space, config_dict))

pbmohpo/utils.py
@@ -1,3 +1,4 @@
+import copy
 from itertools import accumulate
 from typing import Dict, List
 
@@ -6,12 +7,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
-from pbmohpo.archive import Archive
 
-
-def get_botorch_bounds(space: CS.ConfigurationSpace):
+def get_botorch_bounds(space: CS.ConfigurationSpace, on_search_space: bool = True) -> List:
     """
-    Get bounds of hyperparameters in the format botorch needs
+    Get bounds of hyperparameters in the format botorch needs,
+    If `on_search_space` is True, the bounds are returned as on the search space, i.e. respecting log transformations.
 
     Parameters
     ----------
@@ -23,8 +23,37 @@ def get_botorch_bounds(space: CS.ConfigurationSpace):
         list of [lower, upper] bounds of each hyperparameter
     """
     hps = space.get_hyperparameters()
-    bounds = [[hp.lower, hp.upper] for hp in hps]
+    if on_search_space:
+        bounds = [[np.log(hp.lower), np.log(hp.upper)] if hp.log else [hp.lower, hp.upper] for hp in hps]
+    else:
+        bounds = [[hp.lower, hp.upper] for hp in hps]
     return torch.from_numpy(np.array(bounds).T)
+
+
+def get_config_values(config: CS.Configuration, space: CS.ConfigurationSpace, on_search_space: bool = True) -> List:
+    """
+    Get the values of a configuration.
+    If `on_search_space` is True, the values are returned as on the search space, i.e. respecting log transformations.
+    If `on_search_space` is False, the values are returned as on the original space.
+
+    Parameters
+    ----------
+    config: CS.Configuration
+        Configuration to be evaluated
+    search_space: CS.ConfigurationSpace
+        Search space of the problem
+
+    Returns
+    -------
+    List
+        List of values of the configuration
+    """
+    values = copy.deepcopy(config.get_dictionary())
+    if on_search_space:
+        for hp in space.get_hyperparameters():
+            if hp.log:
+                values.update({hp.name: np.log(values[hp.name])})
+    return list(values.values())
 
 
 def remove_hp_from_cs(
@@ -78,7 +107,7 @@ def remove_hp_from_cs(
 
 
 def visualize_archives(
-    archive_list: List[Archive], plot_elements: List[str] = ["incumbent"]
+    archive_list: List["Archive"], plot_elements: List[str] = ["incumbent"], legend_elements: List[str] = None
 ):
     """
     Visualize archive utility and incumbent utility over iterations.
@@ -89,10 +118,14 @@ def visualize_archives(
         List of archives to be visualized
 
     plot_elements: list[str]
-        List of elements that should be plotted. Currently supports
+        List of elements that should be plotted. Currently, supports
         "incumbent", which plots the incumbent utility over iteration and
         "utilities", which plots the utility for each iteration over
         iteration.
+
+    legend_elements: list[str]
+        List of elements that should be included in the legend.
+        Must be of the same length as archive_list.
 
     Returns
     -------
@@ -129,6 +162,8 @@ def visualize_archives(
                 marker="x",
                 alpha=0.3,
             )
+        if len(legend_elements) == len(archive_list):
+            ax.legend(legend_elements)
 
     ax.set(xlabel="Iterations", ylabel="Utility (Not Normalized)")
     return fig